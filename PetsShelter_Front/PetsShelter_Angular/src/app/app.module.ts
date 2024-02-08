980ec3c0e8127ef941751236af34d0c805cfa8fe
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-08 18:25:40 +0100

    Add register form

--- PetsShelter_Front/PetsShelter_Angular/src/app/app.module.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

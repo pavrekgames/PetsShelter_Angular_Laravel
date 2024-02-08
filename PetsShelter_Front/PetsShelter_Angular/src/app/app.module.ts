893f7349a8d978d56ced6c3192f5c43fd8ebc2d2
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-08 20:58:45 +0100

    Add routes and home component as a home page

--- PetsShelter_Front/PetsShelter_Angular/src/app/app.module.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/app.module.ts
@@ -7,21 +7,27 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule, provideRouter } from '@angular/router';
+import routeConfig from './routing/routes';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideRouter(routeConfig)
   ],
   bootstrap: [AppComponent]
 })

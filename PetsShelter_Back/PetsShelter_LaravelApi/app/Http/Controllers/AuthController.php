798f9e941b5af0774ecfa098c4ba0f17eb0d4091
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-11 18:05:14 +0100

    Implement register,login and logout on backend

--- PetsShelter_Back/PetsShelter_LaravelApi/app/Http/Controllers/AuthController.php
+++ PetsShelter_Back/PetsShelter_LaravelApi/app/Http/Controllers/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use Cookie;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -19,4 +22,37 @@ class AuthController extends Controller
             'tokens_count' => $request->input('tokens_count')
         ]);
     }
+
+    public function login(Request $request){
+        if(!Auth::attempt($request->only('email','password'))){
+            return response([
+                'message' => 'Błędny e-mail lub hasło'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('loginToken')->plainTextToken;
+
+        $cookie = cookie('loginToken', $token, 60 * 24);
+
+        return response([
+            'message'=> 'Zostałeś zalogowany'
+        ])->withCookie($cookie);
+    }
+
+    public function user(){
+        return Auth::user();
+    }
+
+    public function logout(){
+        $cookie = Cookie::forget('loginToken');
+
+        //$request->user->remember_token.delete();
+
+        return response([
+            'message'=> 'Zostałeś wylogowany'
+        ])->withCookie($cookie);
+    }
+
 }

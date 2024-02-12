86109a43b8b361dcef510bffef165d617e8209cc
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-12 11:44:56 +0100

    Implement login form validation on frontend

--- PetsShelter_Front/PetsShelter_Angular/src/app/login/login.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/login/login.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+declare let alertify: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  hasSubmitted: boolean = false;
 
-}
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.hasSubmitted = false;
+  }
+
+  onSubmit(){
+    this.hasSubmitted = true;
+
+    if (this.loginForm.valid) {
+   /*   this.http
+        .post(
+          'http://127.0.0.1:8000/api/login',
+          this.loginForm.getRawValue()
+        )
+        .subscribe(() => {
+          this.router.navigate(['/']);
+          alertify.success('Zostałeś zalogowany');
+        }); */
+    }else{
+      console.log("Form is invalid");
+    }
+  }
+
+
+  }
+

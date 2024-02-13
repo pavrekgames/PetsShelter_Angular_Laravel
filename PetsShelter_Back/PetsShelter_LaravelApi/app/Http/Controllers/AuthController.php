189bd9712874411acdbbf36c91a4f3091ac591c4
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-13 17:53:22 +0100

    Add JWT token functions in User model

--- PetsShelter_Back/PetsShelter_LaravelApi/app/Http/Controllers/AuthController.php
+++ PetsShelter_Back/PetsShelter_LaravelApi/app/Http/Controllers/AuthController.php
@@ -12,6 +12,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
+
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
     public function register(Request $request){
 
         $data = $request->only('name', 'surname', 'email', 'password');
@@ -37,9 +48,14 @@ class AuthController extends Controller
         ]);
     }
 
-    public function login(Request $request){
-
-        $credentials = $request->only('email', 'password');
+ /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
 
         $validator = Validator::make($credentials, [
             'email' => 'required|email',
@@ -50,14 +66,59 @@ class AuthController extends Controller
             return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
         }
 
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
     }
 
-    public function user(){
-        return Auth::user();
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
     }
 
-    public function logout(){
+   /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
 
+        return response()->json(['message' => 'Zostałes wylogowany']);
+    }
+
+     /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
     }
 
 }
--- PetsShelter_Back/PetsShelter_LaravelApi/routes/api.php
+++ PetsShelter_Back/PetsShelter_LaravelApi/routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::get('user', [AuthController::class, 'user']);
+//Route::get('user', [AuthController::class, 'user']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);

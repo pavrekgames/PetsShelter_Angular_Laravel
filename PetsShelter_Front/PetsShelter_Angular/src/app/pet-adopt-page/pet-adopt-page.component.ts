c1890817a0c988f8fc44052d5bfdb1fb7c446dc4
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-19 09:52:21 +0100

    Create pet-adopt-page component

--- PetsShelter_Front/PetsShelter_Angular/src/app/pet-adopt-page/pet-adopt-page.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/pet-adopt-page/pet-adopt-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api-service';
 
 @Component({
   selector: 'app-pet-adopt-page',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './pet-adopt-page.component.css'
 })
 export class PetAdoptPageComponent {
+
+  pet: any;
+
+  constructor(private apiService: ApiService) {}
 
 }

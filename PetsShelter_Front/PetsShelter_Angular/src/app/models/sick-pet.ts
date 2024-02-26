956fec615b7df0e3ed2d5723a0394df0389fe3b5
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-26 11:18:53 +0100

    Create sick pet model on frontend

---
+++ PetsShelter_Front/PetsShelter_Angular/src/app/models/sick-pet.ts
@@ -0,0 +1,11 @@
+export interface SickPet{
+  id: number;
+  name: string;
+  species: string;
+  disease: string;
+  current_tokens: number;
+  required_tokens: number;
+  status: string;
+  photo_path: string;
+  id_User: number;
+}

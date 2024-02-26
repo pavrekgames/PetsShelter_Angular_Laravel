a71265573d7620e98bdd68976794a55e7e43e6ff
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-02-26 11:00:23 +0100

    Create model, controller and table for sick pets

---
+++ PetsShelter_Back/PetsShelter_LaravelApi/app/Http/Controllers/SickPetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SickPet;
+use Illuminate\Http\Request;
+
+class SickPetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SickPet  $sickPet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SickPet $sickPet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SickPet  $sickPet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SickPet $sickPet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SickPet  $sickPet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SickPet $sickPet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SickPet  $sickPet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SickPet $sickPet)
+    {
+        //
+    }
+}
---
+++ PetsShelter_Back/PetsShelter_LaravelApi/database/migrations/2024_02_26_095521_create_sick_pets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sick-pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('species');
+            $table->string('disease');
+            $table->integer('current_tokens');
+            $table->integer('required_tokens');
+            $table->string('status');
+            $table->string('photo_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sick_pets');
+    }
+};

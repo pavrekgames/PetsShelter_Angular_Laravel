df31bb70ed9bc4c4c8d2f472f71fb272dae26f02
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-03-01 11:32:12 +0100

    Create conversations migration

---
+++ PetsShelter_Back/PetsShelter_LaravelApi/database/migrations/2024_03_01_102644_create_conversations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('user_sender_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('user_receiver_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('pet_id')->constrained('pets')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+};

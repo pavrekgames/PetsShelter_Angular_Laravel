e3054aa5728078c5f830ab0864660860079ed5a5
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-03-03 11:49:21 +0100

    Move logic from message-list-element to messages-bar component

--- PetsShelter_Front/PetsShelter_Angular/src/app/message-list-element/message-list-element.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/message-list-element/message-list-element.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { ApiService } from '../services/api-service';
 
-declare let alertify: any;
 
 @Component({
   selector: 'app-message-list-element',
@@ -12,29 +10,5 @@ declare let alertify: any;
 export class MessageListElementComponent {
 
   faUser = faUser;
-
-  convsersations: any;
-
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit(): void {
-    this.apiService.getConversations().subscribe({
-      next: (data) => {
-        this.handleConversations(data);
-        console.log(data);
-      },
-      error: (error) => {
-        this.handleError();
-      },
-    });
-  }
-
-  handleConversations(data: any) {
-    this.convsersations = data;
-  }
-
-  handleError() {
-    alertify.error('Wystąpił problem z wyświetleniem konwersjacji');
-  }
 
 }
--- PetsShelter_Front/PetsShelter_Angular/src/app/messages-bar/messages-bar.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/messages-bar/messages-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api-service';
+
+declare let alertify: any;
 
 @Component({
   selector: 'app-messages-bar',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './messages-bar.component.css'
 })
 export class MessagesBarComponent {
+
+  convsersations: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getConversations().subscribe({
+      next: (data) => {
+        this.handleConversations(data);
+        console.log(data);
+      },
+      error: (error) => {
+        this.handleError();
+      },
+    });
+  }
+
+  handleConversations(data: any) {
+    this.convsersations = data;
+  }
+
+  handleError() {
+    alertify.error('Wystąpił problem z wyświetleniem konwersjacji');
+  }
+
 
 }

11ca22746548f8b6682d7e3022a975a01276ec6d
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-03-03 11:44:36 +0100

    Create conversation model and implement getting conversations on frontend

--- PetsShelter_Front/PetsShelter_Angular/src/app/message-list-element/message-list-element.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/message-list-element/message-list-element.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { ApiService } from '../services/api-service';
+
+declare let alertify: any;
 
 @Component({
   selector: 'app-message-list-element',
@@ -9,5 +12,29 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 export class MessageListElementComponent {
 
   faUser = faUser;
+
+  convsersations: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getConversations().subscribe({
+      next: (data) => {
+        this.handleConversations(data);
+        console.log(data);
+      },
+      error: (error) => {
+        this.handleError();
+      },
+    });
+  }
+
+  handleConversations(data: any) {
+    this.convsersations = data;
+  }
+
+  handleError() {
+    alertify.error('Wystąpił problem z wyświetleniem konwersjacji');
+  }
 
 }
---
+++ PetsShelter_Front/PetsShelter_Angular/src/app/models/conversation.ts
@@ -0,0 +1,7 @@
+export interface Conversation{
+  id: number;
+  user_name: string;
+  user_surname: string;
+  pet_name: string;
+  pet_photo: string;
+}

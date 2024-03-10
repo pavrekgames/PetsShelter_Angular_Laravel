ca7b4210a6f8c57d9c2f6ff270973542ba5f5f94
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-03-10 19:36:05 +0100

    Create payments and payments_bundles migrations

---
+++ PetsShelter_Back/PetsShelter_LaravelApi/database/migrations/2024_03_10_183400_create_payments_bundles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments_bundles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('payment_id')->constrained('payments')->cascadeOnDelete();
+            $table->foreignId('bundle_id')->constrained('bundles')->cascadeOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments_bundles');
+    }
+};

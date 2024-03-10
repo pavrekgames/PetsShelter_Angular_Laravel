b9164f3696bffee15fcaa604a98a251589b1642b
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-03-10 15:50:59 +0100

    Create migration, model and controller for bundle

---
+++ PetsShelter_Back/PetsShelter_LaravelApi/app/Models/Bundle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bundle extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'tokens_count',
+        'price',
+        'currency'
+    ];
+
+}

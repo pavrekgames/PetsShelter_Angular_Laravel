95db09265b80bb3209a5600f88d77444ddd21c70
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-03-11 12:42:52 +0100

    Implement card element basics

--- PetsShelter_Front/PetsShelter_Angular/src/app/payment/payment.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/payment/payment.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
 import { Bundle } from '../models/bundle';
 import { faSackDollar } from '@fortawesome/free-solid-svg-icons';
 import { ApiService } from '../services/api-service';
 import { ActivatedRoute } from '@angular/router';
 import { SpinnerService } from '../services/spinner.service';
+import { AngularStripeService } from '@fireflysemantics/angular-stripe-service';
+import  { NgForm } from "@angular/forms"
 
 @Component({
   selector: 'app-payment',
@@ -23,10 +25,23 @@ export class PaymentComponent {
     currency: 'pln',
   };
 
+  @ViewChild('cardInfo', { static: false }) cardInfo: ElementRef;
+
+  stripe: any;
+  loading = false;
+  confirmation: any;
+
+  card: any;
+  cardHandler = this.onChange.bind(this);
+  error: string = '';
+
+
   constructor(
     private apiService: ApiService,
     private route: ActivatedRoute,
-    private spinnerService: SpinnerService
+    private spinnerService: SpinnerService,
+    private cd: ChangeDetectorRef,
+    private stripeService: AngularStripeService
   ) {}
 
   ngOnInit(): void {
@@ -43,8 +58,43 @@ export class PaymentComponent {
 
   }
 
+  ngAfterViewInit() {
+    this.stripeService.setPublishableKey('pk_test_2syov9fTMRwOxYG97AAXbOgt008X6NL46o').then(
+      stripe=> {
+        this.stripe = stripe;
+    const elements = stripe.elements();
+    this.card = elements.create('card');
+    this.card.mount(this.cardInfo.nativeElement);
+    this.card.addEventListener('change', this.cardHandler);
+    });
+  }
+
   handleBundle(data: any) {
     this.bundle = data;
+  }
+
+  ngOnDestroy() {
+    this.card.removeEventListener('change', this.cardHandler);
+    this.card.destroy();
+  }
+
+  onChange({error} : {error:any}) {
+    if (error) {
+      this.error = error.message;
+    } else {
+      this.error = '';
+    }
+    this.cd.detectChanges();
+  }
+
+  async onSubmit(form: NgForm) {
+    const { token, error } = await this.stripe.createToken(this.card);
+
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      console.log('Success!', token);
+    }
   }
 
 }

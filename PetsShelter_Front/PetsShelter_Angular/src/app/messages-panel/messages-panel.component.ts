a6d93304272495709b1d8adfb3297a6493f3de14
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-03-15 17:32:13 +0100

    Implement messages-bar component sidebar behaviour on mobile size

--- PetsShelter_Front/PetsShelter_Angular/src/app/messages-panel/messages-panel.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/messages-panel/messages-panel.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
 import { MessagesService } from '../services/messages.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-messages-panel',
   templateUrl: './messages-panel.component.html',
-  styleUrl: './messages-panel.component.css'
+  styleUrl: './messages-panel.component.css',
 })
 export class MessagesPanelComponent {
+  isMobile: boolean = false;
 
   messagesCount: number = 0;
 
-  constructor(private messagesService: MessagesService) { }
+  constructor(
+    private messagesService: MessagesService,
+    private breakPointService: BreakpointObserver
+  ) {}
 
   ngOnInit() {
     this.messagesService.messagesCountObs.subscribe(
       (messagesCount) => (this.messagesCount = messagesCount)
     );
-  }
 
+    this.breakPointService.observe(Breakpoints.XSmall).subscribe((result) => {
+      this.isMobile = false;
+
+      if (result.matches) {
+        this.isMobile = true;
+      }else{
+        this.isMobile = false;
+      }
+    });
+
+  }
 }

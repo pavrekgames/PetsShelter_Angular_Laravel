21f05d5dcadd3d98a1170055d4fe8046e974d462
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-03-18 12:15:25 +0100

    Refactor edit-pet-photo component

--- PetsShelter_Front/PetsShelter_Angular/src/app/edit-pet-photo/edit-pet-photo.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/edit-pet-photo/edit-pet-photo.component.ts
@@ -10,7 +10,7 @@ declare let alertify: any;
 @Component({
   selector: 'app-edit-pet-photo',
   templateUrl: './edit-pet-photo.component.html',
-  styleUrl: './edit-pet-photo.component.css'
+  styleUrl: './edit-pet-photo.component.css',
 })
 export class EditPetPhotoComponent {
   isMobile: boolean = false;
@@ -23,7 +23,7 @@ export class EditPetPhotoComponent {
   error: any = [];
 
   editPetPhotoForm = this.formBuilder.group({
-    photo: [null, [Validators.required]]
+    photo: [null, [Validators.required]],
   });
 
   constructor(
@@ -39,51 +39,57 @@ export class EditPetPhotoComponent {
     this.petId = this.route.snapshot.params.id;
     this.spinnerService.show();
 
+    this.getPetToEdit();
+    this.checkDeviceSize();
+  }
+
+  getPetToEdit() {
     this.apiPetsService.getPetToEdit(this.petId).subscribe({
       next: (data: any) => {
         this.spinnerService.hide();
         this.handlePetToEdit(data);
-        console.log(data);
       },
     });
+  }
 
+  checkDeviceSize() {
     this.breakPointService.observe(Breakpoints.XSmall).subscribe((result) => {
       this.isMobile = false;
 
       if (result.matches) {
         this.isMobile = true;
-      }else{
+      } else {
         this.isMobile = false;
       }
     });
-
   }
 
   onSubmit() {
     this.hasSubmitted = true;
     this.error = [];
+  }
 
+  validatePetPhotoForm() {
     if (this.editPetPhotoForm.valid) {
       this.spinnerService.show();
-      const formData = this.editPetPhotoForm.getRawValue();
-      console.log("Raw Values: " + JSON.stringify(formData));
 
-      this.apiPetsService.editPetPhoto(this.petId, this.getFormData()).subscribe({
-        next: (data) => {
-          this.spinnerService.hide();
-          this.handleResponse();
-          console.log(data);
-        },
-        error: (error) => {
-          this.spinnerService.hide();
-          this.handleError(error);
-          console.log(this.error);
-        },
-      });
-
+      this.editPetPhoto();
     } else {
       console.log('Form is invalid');
     }
+  }
+
+  editPetPhoto() {
+    this.apiPetsService.editPetPhoto(this.petId, this.getFormData()).subscribe({
+      next: (data) => {
+        this.spinnerService.hide();
+        this.handleResponse();
+      },
+      error: (error) => {
+        this.spinnerService.hide();
+        this.handleError(error);
+      },
+    });
   }
 
   handleResponse() {
@@ -98,27 +104,23 @@ export class EditPetPhotoComponent {
 
   handlePetToEdit(data: any) {
     this.pet = data;
-
   }
 
-  onFileChange(event: any){
-    console.log(event.target.files[0]);
+  onFileChange(event: any) {
     const photoToUpload = event.target.files[0];
-    this.editPetPhotoForm.patchValue({photo: photoToUpload});
+    this.editPetPhotoForm.patchValue({ photo: photoToUpload });
     this.editPetPhotoForm.get('photo')?.updateValueAndValidity();
   }
 
-  getFormData(){
+  getFormData() {
     const formData: any = new FormData();
     formData.append('photo', this.editPetPhotoForm.get('photo')?.value);
 
     return formData;
   }
 
-  cancelEditing(){
+  cancelEditing() {
     this.router.navigate(['/my-pets']);
     alertify.warning('Anulowano zmianę zdjęcia');
   }
-
-
 }

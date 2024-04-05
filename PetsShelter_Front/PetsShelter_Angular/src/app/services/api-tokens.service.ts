9cc44a2743e4d4a363691b37be942eee09ce6e5c
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-04-05 11:41:55 +0200

    Edit api-tokens service by environment api url

--- PetsShelter_Front/PetsShelter_Angular/src/app/services/api-tokens.service.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/services/api-tokens.service.ts
@@ -1,57 +1,54 @@
 import { Injectable } from '@angular/core';
 import { TokenService } from './token.service';
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiTokensService {
+  private API_URL = environment.API_URL;
 
   constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  transferTokens(petId: any, data: any){
+  transferTokens(petId: any, data: any) {
     const token = this.tokenService.getTokenValue();
-    const headers = new HttpHeaders().set('Authorization', "Bearer " + token);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
 
-    return this.http.post('http://127.0.0.1:8000/api/transfer-tokens/' + petId, data, {'headers': headers});
-
+    return this.http.post(this.API_URL + 'transfer-tokens/' + petId, data, {
+      headers: headers,
+    });
   }
 
-  getbundles(){
-
+  getbundles() {
     const token = this.tokenService.getTokenValue();
-    const headers = new HttpHeaders().set('Authorization', "Bearer " + token);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
 
-    return this.http.get('http://127.0.0.1:8000/api/bundles', {'headers': headers});
-
+    return this.http.get(this.API_URL + 'bundles', { headers: headers });
   }
 
-  getbundle(id: any){
-
+  getbundle(id: any) {
     const token = this.tokenService.getTokenValue();
-    const headers = new HttpHeaders().set('Authorization', "Bearer " + token);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
 
-    return this.http.get('http://127.0.0.1:8000/api/bundles/' + id, {'headers': headers});
-
+    return this.http.get(this.API_URL + 'bundles/' + id, { headers: headers });
   }
 
-  createPayIntent(data: any){
-
+  createPayIntent(data: any) {
     const token = this.tokenService.getTokenValue();
-    const headers = new HttpHeaders().set('Authorization', "Bearer " + token);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
 
-    return this.http.post('http://127.0.0.1:8000/api/payment-intent', data, {'headers': headers});
-
+    return this.http.post(this.API_URL + 'payment-intent', data, {
+      headers: headers,
+    });
   }
 
-  storePayment(data: any){
-
+  storePayment(data: any) {
     const token = this.tokenService.getTokenValue();
-    const headers = new HttpHeaders().set('Authorization', "Bearer " + token);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
 
-    return this.http.post('http://127.0.0.1:8000/api/store-payment', data, {'headers': headers});
-
+    return this.http.post(this.API_URL + 'store-payment', data, {
+      headers: headers,
+    });
   }
-
-
 }

5c344b7c889b18315e986dd7d32907d98b14c343
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-04-10 10:10:44 +0200

    Configure backend and url for deploy

--- PetsShelter_Front/PetsShelter_Angular/src/app/services/token.service.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/services/token.service.ts
@@ -1,31 +1,35 @@
 import { Injectable } from '@angular/core';
 import { LocalstorageService } from '../LocalStorage';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenService {
-  constructor(private localStorage: LocalstorageService) {}
+
+  private API_URL = environment.API_URL;
+
+  //constructor(private localStorage: LocalstorageService) {}
 
   handleToken(token: any) {
     this.setToken(token);
   }
 
   setToken(token: any) {
-    //return localStorage.setItem('token', token);
-    return this.localStorage.setItem('token', token);
+    return localStorage.setItem('token', token);
+    //return this.localStorage.setItem('token', token);
   }
 
   getToken() {
-    //return localStorage.getItem('token');
+    return localStorage.getItem('token');
 
-    return this.localStorage.getItem('token');
+    //return this.localStorage.getItem('token');
   }
 
   removeToken() {
-    //return localStorage.removeItem('token');
-    this.localStorage.removeItem('token');
+    return localStorage.removeItem('token');
+    //this.localStorage.removeItem('token');
   }
 
   isTokenValid() {
@@ -34,7 +38,7 @@ export class TokenService {
     if (token) {
       const payload = this.payload(token);
       if (payload) {
-        return payload.iss === 'http://127.0.0.1:8000/api/login' ? true : false;
+        return payload.iss === this.API_URL + 'login' ? true : false;
       }
     }
     return false;

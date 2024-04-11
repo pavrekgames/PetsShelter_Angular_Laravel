80c9146ed8ac0ebcb4a2f1cbd218bb3c87bfd52c
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-04-11 11:27:20 +0200

    Change put and delete methods for post on frontend for deploy

--- PetsShelter_Front/PetsShelter_Angular/src/environments/environment.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //API_URL: 'https://petsshelter.free.nf/api/',
-  API_URL: 'http://127.0.0.1:8000/api/',
+  API_URL: 'https://petsshelter.free.nf/api/',
+  //API_URL: 'http://127.0.0.1:8000/api/',
 };

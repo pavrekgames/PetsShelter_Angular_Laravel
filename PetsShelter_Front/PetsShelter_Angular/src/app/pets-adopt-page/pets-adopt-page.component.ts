c35f929f0358c0728cb9ef7b9a9b1d48a68d64fd
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-07-11 13:05:35 +0200

    Implement query params on frontend

--- PetsShelter_Front/PetsShelter_Angular/src/app/pets-adopt-page/pets-adopt-page.component.ts
+++ PetsShelter_Front/PetsShelter_Angular/src/app/pets-adopt-page/pets-adopt-page.component.ts
@@ -3,6 +3,11 @@ import { SpinnerService } from '../services/spinner.service';
 import { ApiPetsService } from '../services/api-pets.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
+interface QueryParams {
+  size?: string;
+  page?: string;
+}
+
 @Component({
   selector: 'app-pets-adopt-page',
   templateUrl: './pets-adopt-page.component.html',
@@ -32,12 +37,19 @@ export class PetsAdoptPageComponent {
     this.getPetToAdopt();
 
     this.route.queryParamMap.subscribe((params) => {
-      const size = params.get('size');
+      const queryParams: QueryParams = {
+        size: params.get('size')!,
+        page: params.get('page')!,
+      };
 
       if (this.selectedSize) {
         this.filteredPets = this.pets.filter((pet) => {
-          return pet.size === size;
+          return pet.size === queryParams.size;
         });
+      }
+
+      if (this.page > 0) {
+        this.page = +queryParams.page;
       }
     });
   }
@@ -65,6 +77,17 @@ export class PetsAdoptPageComponent {
       queryParams.size = this.selectedSize;
       this.filteredPets = this.pets;
     }
+
+    this.router.navigate([], {
+      queryParams: queryParams,
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  filteredByPage() {
+    const queryParams: any = {};
+
+    queryParams.page = this.page;
 
     this.router.navigate([], {
       queryParams: queryParams,
